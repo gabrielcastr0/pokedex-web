d511c377ea13a498e9ba2598eb93b24dd9a627bb
Gabriel Castro <gabrielcastro.xy@gmail.com>
2022-02-15 16:41:40 -0300

    fix: Retirando imports que não estão sendo usados

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable react/no-array-index-key */
 import "./App.css";
-import Container from "@mui/material/Container";
 
 import { HeaderComponent } from "./components/HeaderComponent";
-import { PokemonsList } from "./pages/PokemonsList";
 import { MainRoutes } from "./routes/MainRoutes";
 
 export function App() {
